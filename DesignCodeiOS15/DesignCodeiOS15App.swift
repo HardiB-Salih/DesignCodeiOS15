9991c331e691f33215a3b937b0ebfe7cf8079531
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2022-11-28 22:39:25 +0300

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by HardiBSalih on 28.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
